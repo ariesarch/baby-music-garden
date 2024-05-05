86c264df24154b97625b94c34361dee96a59ba8f
kyawyenaing <kyawyenaing.dev@gmail.com>
2024-05-05 18:30:04 +0700

    load music online

--- src/store/music.ts
+++ src/store/music.ts
@@ -1,11 +1,11 @@
 import { create } from 'zustand'
 // import Sample from "http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg";
 // import Sample from '/public/audio/sample.mp3'
-import WaterMark from '../audio/Enya - 1988 Watermark - 01 - Watermark.mp3'
-import Perficio from "../audio/Enya - 1988 Watermark - 02 - Cursum Perficio.mp3"
-import OnYourShore from "../audio/Enya - 1988 Watermark - 03 - On Your Shore.mp3"
-import StormsInAfrica from "../audio/Enya - 1988 Watermark - 04 - Storms In Africa.mp3"
-import Exile from "../audio/Enya - 1988 Watermark - 05 - Exile.mp3"
+// import WaterMark from '../audio/Enya - 1988 Watermark - 01 - Watermark.mp3'
+// import Perficio from "../audio/Enya - 1988 Watermark - 02 - Cursum Perficio.mp3"
+// import OnYourShore from "../audio/Enya - 1988 Watermark - 03 - On Your Shore.mp3"
+// import StormsInAfrica from "../audio/Enya - 1988 Watermark - 04 - Storms In Africa.mp3"
+// import Exile from "../audio/Enya - 1988 Watermark - 05 - Exile.mp3"
 // import MissClareRemembers from "../audio/Enya - 1988 Watermark - 06 - Miss Clare Remembers.mp3"
 // import OrinocoFlow from "../audio/Enya - 1988 Watermark - 07 - Orinoco Flow.mp3"
 // import EveningFalls from "../audio/Enya - 1988 Watermark - 08 - Evening Falls.mp3"
@@ -13,34 +13,34 @@ import Exile from "../audio/Enya - 1988 Watermark - 05 - Exile.mp3"
 // import TheLongships from "../audio/Enya - 1988 Watermark - 10 - The Longships.mp3"
 // import NaLaethaGealMOigev from "../audio/Enya - 1988 Watermark - 11 - Na Laetha Geal MOige.mp3"
 // import ShepherdMoons from "../audio/Enya - 1991 Shepherd Moons - 01 - Shepherd Moons.mp3"
-import Flute from "../audio/Flute.mp3"
-import GentleRain from "../audio/Gentle-rain-and-rolling-thunder-sound.mp3"
+// import Flute from "../audio/Flute.mp3"
+// import GentleRain from "../audio/Gentle-rain-and-rolling-thunder-sound.mp3"
 const audioState = [
     {
         id:1,
         name: "Enya - 1988 Watermark - 01 - Watermark",
         // src: "http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg"
-        src: WaterMark
+        src: "https://ia601007.us.archive.org/21/items/1Enya1/Enya%20-%201988%20Watermark%20-%2001%20-%20Watermark.mp3"
     },
     {
         id:2,
         name: "Enya - 1988 Watermark - 02 - Cursum Perficio",
-        src: Perficio
+        src: "https://ia601007.us.archive.org/21/items/1Enya1/Enya%20-%201988%20Watermark%20-%2002%20-%20Cursum%20Perficio.mp3"
     },
     {
         id:3,
         name: "Enya - 1988 Watermark - 03 - On Your Shore",
-        src: OnYourShore
+        src: "https://ia601007.us.archive.org/21/items/1Enya1/Enya%20-%201988%20Watermark%20-%2003%20-%20On%20Your%20Shore.mp3"
     },
     {
         id:4,
         name: "Enya - 1988 Watermark - 04 - Storms In Africa",
-        src: StormsInAfrica
+        src: "https://ia601007.us.archive.org/21/items/1Enya1/Enya%20-%201988%20Watermark%20-%2004%20-%20Storms%20In%20Africa.mp3"
     },
     {
         id:5,
         name: "Enya - 1988 Watermark - 05 - Exile",
-        src: Exile
+        src: "https://ia601007.us.archive.org/21/items/1Enya1/Enya%20-%201988%20Watermark%20-%2005%20-%20Exile.mp3"
     },
     {
         id:6,
@@ -77,15 +77,15 @@ const audioState = [
         name: "Enya - 1991 Shepherd Moons - 01 - Shepherd Moons",
         src: "https://ia601007.us.archive.org/21/items/1Enya1/Enya%20-%201991%20Shepherd%20Moons%20-%2001%20-%20Shepherd%20Moons.mp3"
     },
-    {
-        id: 13,
-        name: "Flute",
-        src: Flute
-    },
-    {
-        id: 14,
-        name: "GentleRain",
-        src: GentleRain
-    }
+    // {
+    //     id: 13,
+    //     name: "Flute",
+    //     src: Flute
+    // },
+    // {
+    //     id: 14,
+    //     name: "GentleRain",
+    //     src: GentleRain
+    // }
 ]
 export default audioState;
